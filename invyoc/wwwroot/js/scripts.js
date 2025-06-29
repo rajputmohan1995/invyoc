c112dc620ca3013e3e1205edd8858f59ed705ba8
Mohan Rajput <rajputmohan1995@gmail.com>
2025-06-29 22:05:01 +0530

    make discount & tax inputs allow numeric value with 2 decimal points

--- invyoc/wwwroot/js/scripts.js
+++ invyoc/wwwroot/js/scripts.js
@@ -12,20 +12,38 @@ $("#spanCurrentYear").text(new Date().getFullYear());
 function isNumberKey(evt, id) {
     try {
         var charCode = (evt.which) ? evt.which : event.keyCode;
+        var input = document.getElementById(id);
+        var value = input.value;
 
+        // Allow only one dot (.)
         if (charCode == 46) {
-            var txt = document.getElementById(id).value;
-            if (!(txt.indexOf(".") > -1)) {
-
+            if (value.indexOf('.') === -1) {
                 return true;
+            } else {
+                return false;
             }
         }
-        if (charCode > 31 && (charCode < 48 || charCode > 57))
-            return false;
 
-        return true;
-    } catch (w) {
-        alert(w);
+        // Allow digits (0-9)
+        if (charCode >= 48 && charCode <= 57) {
+            // If dot exists, check decimal length
+            var dotIndex = value.indexOf('.');
+            if (dotIndex > -1) {
+                var decimals = value.substring(dotIndex + 1);
+                // Prevent entering more than 2 decimal places
+                if (input.selectionStart > dotIndex && decimals.length >= 2) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        // Block other characters
+        return false;
+
+    } catch (e) {
+        console.log(e);
+        return false;
     }
 }
 
