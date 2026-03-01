572f221ffb3da87ecbf6b79482d235c7571ea595
Mohan Rajput <rajputmohan1995@gmail.com>
2026-03-01 14:50:01 +0530

    add required blogs

---
+++ invyoc/Controllers/BlogController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace invyoc.Controllers;
+
+[Route("gst-invoice-format")]
+public class BlogController : Controller
+{
+    [Route("excel")]
+    public IActionResult Excel()
+    {
+        return View();
+    }
+
+    [Route("pdf")]
+    public IActionResult Pdf()
+    {
+        return View();
+    }
+}
